c27872501d192ffedcd81dd6b378a0da7552b243
stelchenko <stelchenkoav@gmail.com>
2021-12-17 14:21:49 +0300

    pokemons by types

--- src/screens/DetailsScreen/components/typeCard/index.tsx
+++ src/screens/DetailsScreen/components/typeCard/index.tsx
@@ -1,13 +1,15 @@
 import React, {FC} from "react";
-import {View, Text, TouchableOpacity} from "react-native";
+import {Text, TouchableOpacity} from "react-native";
 import {TypeBorderColors, TypeCardProps, TypeColors} from "./type";
 import {styles} from "./style";
-import Svg from "react-native-svg";
 
 const TypeCard: FC<TypeCardProps> = props => {
   const {typeName, navigationHandler} = props
   return (
-    <TouchableOpacity onPress={() => navigationHandler('TypePokemons', {typeName: typeName})} style={[styles.card, {backgroundColor: TypeColors[typeName ?? 'default'], borderColor: TypeBorderColors[typeName]}]}>
+    <TouchableOpacity onPress={() => navigationHandler('TypePokemons', {typeName: typeName})} style={[styles.card, {
+      backgroundColor: TypeColors[typeName ?? 'default'],
+      borderColor: TypeBorderColors[typeName]
+    }]}>
       <Text style={styles.typeName}>
         {typeName?.toUpperCase()}
       </Text>
--- src/screens/HomeScreen/components/pokemonList/index.tsx
+++ src/screens/HomeScreen/components/pokemonList/index.tsx
@@ -13,17 +13,21 @@ import {PokemonListProps} from "./type";
 const PokemonList: FC<PokemonListProps> = props => {
   const {navigationHandler, typeName} = props
   console.log(typeName)
-  const pokemonsData = typeName ? useGetPokemonsByTypeQuery({variables: {typeName: typeName}}) : useGetPokemonsQuery({variables: {offset: 0}, fetchPolicy: "network-only"})
+  const pokemonsData = typeName ? useGetPokemonsByTypeQuery({
+    variables: {typeName: typeName, offset: 0},
+    fetchPolicy: "network-only"
+  }) : useGetPokemonsQuery({variables: {offset: 0}})
   if (pokemonsData.loading) return <DefaultActivityIndicator/>
   if (pokemonsData.error) return <Text>ERROR!</Text>
 
   const loadMore = (data: QueryResult) => data.fetchMore({
-    variables: { offset: data.data?.pokemon_v2_pokemon.length},
+    variables: {offset: data.data?.pokemon_v2_pokemon.length},
   })
 
   return (
     <View>
-      <PokemonListView navigationHandler={navigationHandler} pokemonsData={pokemonsData} pokemons={mapPokemonList(pokemonsData.data?.pokemon_v2_pokemon)} loadMore={loadMore}/>
+      <PokemonListView navigationHandler={navigationHandler} pokemonsData={pokemonsData}
+                       pokemons={mapPokemonList(pokemonsData.data?.pokemon_v2_pokemon)} loadMore={loadMore}/>
     </View>
 
   )
